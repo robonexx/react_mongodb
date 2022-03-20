ea5b7cdc630092002be05a4947fdd735777cffc2
robonexx <robertwagar@gmail.com>
2022-03-20 09:53:15 +0100

    express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Registers a user');
-});
+router.post(
+  '/',
+
+  // checking that there is name, email and password / validation
+  [
+    body('name', 'name is required').not().isEmpty(),
+    body('email', 'please include a valid email').isEmail(),
+    body(
+      'password',
+      'Please enter a password with atleast 6 characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
+
+    /*  User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    }).then(user => res.json(user)); */
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 
 const app = express();
 
 // connect db
-connectDB()
+connectDB();
+
+// middleware
+
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.send({ msg: 'Welcome to contact keeper API'})
-})
+  res.send({ msg: 'Welcome to contact keeper API' });
+});
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
@@ -17,4 +21,4 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
