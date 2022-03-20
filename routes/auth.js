9611e2c8145e555864e73037a7b1cf4dd7add3dc
robonexx <robertwagar@gmail.com>
2022-03-20 11:03:28 +0100

    Saving a user and login done, working

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,23 @@ const router = express.Router();
 const bcrypt = require('bcryptjs/dist/bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+
+const auth = require('../middleware/auth');
 const { body, validationResult } = require('express-validator');
+
 const User = require('../models/User');
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 // @route   POST api/auth
